91f45b9a70a235d5e18d6f3e53d75c04badcd005
huevangxp <huevang933@gmail.com>
2026-05-29 14:41:27 +0700

    commit Fri 05/29/2026 14:41:27.14

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore hic
-      architecto nulla in atque, ex quasi optio aliquid soluta esse!
-    </h1>
+    <h1>Huevangxp</h1>
   </div>
 </template>

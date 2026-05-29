d8dc9afe8f73e3dd5e05ffc6805a5c5afc386a60
huevangxp <huevang933@gmail.com>
2026-05-29 14:19:27 +0700

    commit Fri 05/29/2026 14:19:27.17

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,13 +29,5 @@ export default defineNuxtConfig({
     workbox: {
       globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
     },
-    // client: {
-    //   installPrompt: true,
-    // },
-    // devOptions: {
-    //   enabled: true,
-    //   suppressWarnings: true,
-    //   type: "module",
-    // },
   },
 });
